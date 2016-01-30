cf8264e76dbafbf2b8638111bbaa7648e3fa42b0
Sander 3 <JackAc-@hotmail.com>
2016-01-30 02:50:03 +0100

    Created rock falling functions

---
+++ UnityProject/Assets/Design/DesignerScripts/FallingRock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingRock : MonoBehaviour {
+
+	public GameObject particleEffectRef;
+	public GameObject painSphereRef; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Ground") {
+			//Instantiate(particleEffectRef, transform.position, Quaternion.identity);
+			//Instantiate(painSphereRef, transform.position, Quaternion.identity);
+			Destroy (gameObject); 
+		}
+
+	}
+}
---
+++ UnityProject/Assets/Design/DesignerScripts/FallingRocksRandomizer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingRocksRandomizer : MonoBehaviour {
+
+	public float fallingHight = 20f; 
+	public float xRange, zRange; 
+	float transformX, transformZ; 
+
+	public GameObject fallingRockRef; 
+
+
+	// Use this for initialization
+	void Start () {
+		transformX = transform.position.x + xRange;
+		transformZ = transform.position.z + zRange;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			SpawnRandomLocationF (); 
+		}
+	}
+
+
+
+	void SpawnRandomLocationF(){
+		Vector3 SpawnLocation = new Vector3 (Random.Range (transformX, -transformX), fallingHight, Random.Range (transformZ, -transformZ)); 
+		Instantiate(fallingRockRef, SpawnLocation, Quaternion.identity);
+	}
+
+}
